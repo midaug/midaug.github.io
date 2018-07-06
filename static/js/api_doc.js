692e5007692a34dbb362d33a3412c7cff3e66112
midaug <days0814@gmail.com>
2018-07-06 10:37:23 +0800

    Daily updated：2018-07-06 10:37:21 by Midaug personal PC

--- static/js/api_doc.js
+++ static/js/api_doc.js
@@ -1,3 +1,3 @@
-// build time:Fri Jul 06 2018 10:22:05 GMT+0800 (中国标准时间)
+// build time:Fri Jul 06 2018 10:37:08 GMT+0800 (中国标准时间)
 console.log("midaug","---",(new Date).getTime());
 //rebuild by neat 
